385525c7fd50cde234818d2cd7202db923930463
mariomelchor <mario@mariomelchor.com>
2018-05-27 14:54:53 -0700

    Updated app.js, Added counter-example.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,89 @@
 
 console.log('Running');
 
-// JSX
-var template = React.createElement(
-  'h1',
-  null,
-  'Does this change'
-);
+var app = {
+  title: 'Indecision APP',
+  subtitle: 'Put your life in the hands of the computer',
+  options: []
+};
+
+function options(arr) {
+  if (arr.length > 0) {
+    return React.createElement(
+      'p',
+      null,
+      'Here are your Options: ',
+      arr.join(', ')
+    );
+  } else {
+    return React.createElement(
+      'p',
+      null,
+      'No Options'
+    );
+  }
+}
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+
+    e.target.elements.option.value = '';
+    renderTemplate();
+  }
+};
+
+var removeAll = function removeAll() {
+  app.options = [];
+  renderTemplate();
+};
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderTemplate = function renderTemplate() {
+
+  // JSX
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subtitle ? React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ) : '',
+    options(app.options),
+    React.createElement(
+      'p',
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      'button',
+      { onClick: removeAll },
+      'Remove all'
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        { type: 'submit' },
+        'Add Option'
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+renderTemplate();
--- src/app.js
+++ src/app.js
@@ -1,7 +1,59 @@
 console.log('Running')
 
-// JSX
-var template = <h1>Does this change</h1>
-var appRoot = document.getElementById('app');
+const app = {
+  title: 'Indecision APP',
+  subtitle: 'Put your life in the hands of the computer',
+  options: []
+}
 
-ReactDOM.render(template, appRoot);
+function options(arr) {
+  if( arr.length > 0 ) {
+    return <p>Here are your Options: {arr.join(', ')}</p>
+  } else {
+    return <p>No Options</p>
+  }
+}
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  
+  const option = e.target.elements.option.value;
+
+  if( option ) {
+    app.options.push(option);
+
+    e.target.elements.option.value = '';
+    renderTemplate();
+  }
+}
+
+const removeAll = () => {
+  app.options = [];
+  renderTemplate();
+}
+
+const appRoot = document.getElementById('app');
+
+const renderTemplate = () => {
+
+  // JSX
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      { app.subtitle ? <p>{ app.subtitle}</p> : ''}
+      { options(app.options)  }
+      <p>{ app.options.length }</p>
+
+      <button onClick={removeAll}>Remove all</button>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button type="submit">Add Option</button>
+      </form>
+    </div>
+  );
+  
+  ReactDOM.render( template, appRoot);
+
+}
+
+renderTemplate();
