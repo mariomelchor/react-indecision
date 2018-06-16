c5f45e1b17a634d54f424306153a7de115778a8c
mariomelchor <mario@mariomelchor.com>
2018-06-15 22:05:11 -0700

    Added styling, Added scss files, Added favicon

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -72,14 +72,19 @@ class IndecisionApp extends React.Component {
   render() {
 
     const title = 'Indecision';
-    const subtitle = 'Put your life in the hads of a computer';
+    const subtitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
         <Header title={title} subtitle={subtitle} />
-        <Action hasOptions={this.state.options.length > 0 } handlePick={this.handlePick}/>
-        <Options options={this.state.options} handleDeleteOptions={this.handleDeleteOptions} handleDeleteOption={this.handleDeleteOption} />
-        <AddOption handleAddOption={this.handleAddOption} />
+        <div className="container">
+          <Action hasOptions={this.state.options.length > 0 } handlePick={this.handlePick}/>
+
+          <div className="widget">
+            <Options options={this.state.options} handleDeleteOptions={this.handleDeleteOptions} handleDeleteOption={this.handleDeleteOption} />
+            <AddOption handleAddOption={this.handleAddOption} />
+          </div>
+        </div>
         <OptionModal selectedOption={this.state.selectedOption} handleClearSelectedOption={this.handleClearSelectedOption} />
       </div>
     )
